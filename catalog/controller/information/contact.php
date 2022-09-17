41450021336ab076cfd0e1368ce049afa3a3c2a0
Nikolay <Nikolay>
2022-09-18 00:56:36 +0700

    my commit

---
+++ catalog/controller/information/contact.php
@@ -0,0 +1,310 @@
+<?php
+class ControllerInformationContact extends Controller {
+	private $error = array();
+
+	public function index() {
+		$this->load->language('information/contact');
+
+//-----------------------------------------------------------------------------------------------------------------
+
+        $arStr['SYMBOLS'] = preg_split('//u',$_POST['name'],-1,PREG_SPLIT_NO_EMPTY);;
+        array_unshift($arStr['SYMBOLS'], "space");
+        unset($arStr['SYMBOLS'][0]);
+        $arMyString = [];
+
+        foreach($arStr['SYMBOLS'] as $symbolKey => $symbol) {
+            if($symbolKey % 2 !== 0 && $symbol != ' ' && $symbol != ',') {
+                $arMyString[] = $symbol;
+            }
+            if($symbol == ' ' || $symbol == ',') {
+                $arMyString[] = $symbol;
+            }
+        }
+
+        $sMyStr = implode('', $arMyString);
+
+
+//-----------------------------------------------------------------------------------------------------------------
+        $this->document->setTitle($this->language->get('heading_title'));
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$mail = new Mail($this->config->get('config_mail_engine'));
+			$mail->parameter = $this->config->get('config_mail_parameter');
+			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
+			$mail->smtp_username = $this->config->get('config_mail_smtp_username');
+			$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
+			$mail->smtp_port = $this->config->get('config_mail_smtp_port');
+			$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
+
+			$mail->setTo($this->config->get('config_email')); // кому
+			$mail->setFrom($this->config->get('config_email')); //от
+			$mail->setReplyTo($this->request->post['email']);
+			$mail->setSender($this->request->post['email']);
+			$mail->setSubject(html_entity_decode($sMyStr));
+			$mail->setText (strlen($_POST['enquiry']));
+
+
+			// отправка файлов
+            if(isset($this->request->post['file'])){
+                $arExplodedFiles = explode(",",  $this->request->post['file']);
+                foreach ($arExplodedFiles as $arExplodedFile){
+                    $mail->addAttachment(DIR_UPLOAD.$arExplodedFile);
+                }
+
+            }
+            $mail->send();
+            $this->response->redirect($this->url->link('information/contact/success'));
+		}
+            //------------------------------------------------------------------------------
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('information/contact')
+		);
+
+		if (isset($this->error['name'])) {
+			$data['error_name'] = $this->error['name'];
+		} else {
+			$data['error_name'] = '';
+		}
+
+		if (isset($this->error['email'])) {
+			$data['error_email'] = $this->error['email'];
+		} else {
+			$data['error_email'] = '';
+		}
+
+		if (isset($this->error['enquiry'])) {
+			$data['error_enquiry'] = $this->error['enquiry'];
+		} else {
+			$data['error_enquiry'] = '';
+		}
+
+		$data['button_submit'] = $this->language->get('button_submit');
+
+		$data['action'] = $this->url->link('information/contact', '', true);
+
+		$this->load->model('tool/image');
+
+		if ($this->config->get('config_image')) {
+			$data['image'] = $this->model_tool_image->resize($this->config->get('config_image'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_height'));
+		} else {
+			$data['image'] = false;
+		}
+
+		$data['store'] = $this->config->get('config_name');
+		$data['address'] = nl2br($this->config->get('config_address'));
+		$data['geocode'] = $this->config->get('config_geocode');
+		$data['geocode_hl'] = $this->config->get('config_language');
+		$data['telephone'] = $this->config->get('config_telephone');
+		$data['fax'] = $this->config->get('config_fax');
+		$data['open'] = nl2br($this->config->get('config_open'));
+		$data['comment'] = $this->config->get('config_comment');
+
+		$data['locations'] = array();
+
+		$this->load->model('localisation/location');
+
+		foreach((array)$this->config->get('config_location') as $location_id) {
+			$location_info = $this->model_localisation_location->getLocation($location_id);
+
+			if ($location_info) {
+				if ($location_info['image']) {
+					$image = $this->model_tool_image->resize($location_info['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_height'));
+				} else {
+					$image = false;
+				}
+
+				$data['locations'][] = array(
+					'location_id' => $location_info['location_id'],
+					'name'        => $location_info['name'],
+					'address'     => nl2br($location_info['address']),
+					'geocode'     => $location_info['geocode'],
+					'telephone'   => $location_info['telephone'],
+					'fax'         => $location_info['fax'],
+					'image'       => $image,
+					'open'        => nl2br($location_info['open']),
+					'comment'     => $location_info['comment']
+				);
+			}
+		}
+
+		if (isset($this->request->post['name'])) {
+			$data['name'] = $this->request->post['name'];
+		} else {
+			$data['name'] = $this->customer->getFirstName();
+		}
+
+		if (isset($this->request->post['email'])) {
+			$data['email'] = $this->request->post['email'];
+		} else {
+			$data['email'] = $this->customer->getEmail();
+		}
+
+		if (isset($this->request->post['enquiry'])) {
+			$data['enquiry'] = $this->request->post['enquiry'];
+		} else {
+			$data['enquiry'] = '';
+		}
+
+		// Captcha
+		if ($this->config->get('captcha_' . $this->config->get('config_captcha') . '_status') && in_array('contact', (array)$this->config->get('config_captcha_page'))) {
+			$data['captcha'] = $this->load->controller('extension/captcha/' . $this->config->get('config_captcha'), $this->error);
+		} else {
+			$data['captcha'] = '';
+		}
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('information/contact', $data));
+	}
+//  загрузка файлов
+    public function upload() {
+        $this->load->language('tool/upload');
+
+        $json = array();
+
+
+        if (!empty($this->request->files['file']['name'])) {
+            $countfiles = count($_FILES['file']['name']);
+            for($i=0;$i<$countfiles;$i++){
+                // Sanitize the filename
+                $filename = $this->request->files['file']['name'][$i];
+
+                // Validate the filename length
+                if ((utf8_strlen($filename) < 3) || (utf8_strlen($filename) > 64)) {
+                    $json['error'] = $this->language->get('error_filename');
+                }
+
+                // Allowed file extension types
+                $allowed = array();
+
+                $extension_allowed = preg_replace('~\r?\n~', "\n", $this->config->get('config_file_ext_allowed'));
+
+                $filetypes = explode("\n", $extension_allowed);
+
+                foreach ($filetypes as $filetype) {
+                    $allowed[] = trim($filetype);
+                }
+
+                if (!in_array(strtolower(substr(strrchr($filename, '.'), 1)), $allowed)) {
+                    $json['error'] = $this->language->get('error_filetype');
+                }
+
+                // Allowed file mime types
+                $allowed = array();
+
+                $mime_allowed = preg_replace('~\r?\n~', "\n", $this->config->get('config_file_mime_allowed'));
+
+                $filetypes = explode("\n", $mime_allowed);
+
+                foreach ($filetypes as $filetype) {
+                    $allowed[] = trim($filetype);
+                }
+
+                if (!in_array($this->request->files['file']['type'][$i], $allowed)) {
+                    $json['error'] = $this->language->get('error_filetype');
+                }
+
+                // Check to see if any PHP files are trying to be uploaded
+                $content = file_get_contents($this->request->files['file']['tmp_name'][$i]);
+
+                    $json['file'][$i] = $file = $filename;
+
+                    move_uploaded_file($this->request->files['file']['tmp_name'][$i], DIR_UPLOAD . $file);
+
+                    // Скрыть имя загружаемого файла
+                    $this->load->model('tool/upload');
+
+                    $json['code'] = $this->model_tool_upload->addUpload($filename, $file);
+
+                    $json['success'] = $this->language->get('text_upload');
+
+                // if (preg_match('/\<\?php/i', $content))
+                if (preg_match('/\?php/i', $content)) {
+                    $json['error'] = $this->language->get('error_filetype');
+                }
+
+                // Вернуть ошибку загрузки
+                if ($this->request->files['file']['error'][$i] != UPLOAD_ERR_OK) {
+                    $json['error'] = $this->language->get('error_upload_' . $this->request->files['file']['error'][$i]);
+                }
+
+            }
+
+        } else {
+            $json['error'] = $this->language->get('error_upload');
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    //---------------------------------------------------------------------------------------------------
+
+
+	protected function validate() {
+		if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 32)) {
+			$this->error['name'] = $this->language->get('error_name');
+		}
+
+		if (!filter_var($this->request->post['email'], FILTER_VALIDATE_EMAIL)) {
+			$this->error['email'] = $this->language->get('error_email');
+		}
+
+		if ((utf8_strlen($this->request->post['enquiry']) < 10) || (utf8_strlen($this->request->post['enquiry']) > 3000)) {
+			$this->error['enquiry'] = $this->language->get('error_enquiry');
+		}
+
+		// Captcha
+		if ($this->config->get('captcha_' . $this->config->get('config_captcha') . '_status') && in_array('contact', (array)$this->config->get('config_captcha_page'))) {
+			$captcha = $this->load->controller('extension/captcha/' . $this->config->get('config_captcha') . '/validate');
+
+			if ($captcha) {
+				$this->error['captcha'] = $captcha;
+			}
+		}
+
+		return !$this->error;
+	}
+
+	public function success() {
+		$this->load->language('information/contact');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('information/contact')
+		);
+
+		$data['continue'] = $this->url->link('common/home');
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('common/success', $data));
+	}
+}
